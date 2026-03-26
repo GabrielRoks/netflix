bb59bc0f05732118d7b2b3f61e5a7a407cfb57eb
Gabriel <gabrielrokssz@gmail.com>
2026-03-26 11:42:47 -0300

    JavaScript Add

---
+++ index.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('light-mode');
+    if (body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+window.onload = function() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'light') {
+        document.body.classList.add('light-mode');
+    }
+};
